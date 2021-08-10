09d48660e9443262d16e99e13a59c436c6522128
yanghaiyang <1728239753@qq.com>
2021-08-10 16:10:02 +0800

    second commit

--- src/com/yhy/test/TestPerson.java
+++ src/com/yhy/test/TestPerson.java
@@ -3,5 +3,6 @@ package com.yhy.test;
 public class TestPerson {
     public static void main(String[] args) {
         System.out.println("我是测试类！");
+        System.out.println("我被clone了一次");
     }
 }
